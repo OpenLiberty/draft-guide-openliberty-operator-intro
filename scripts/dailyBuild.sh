64e5f3bce0e900f3707f4f838d32ebe9295e81bb
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-04 09:32:04 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -14,9 +14,4 @@ echo "Testing daily build image"
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml
 cat system/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile
-cat system/Dockerfile
-
-docker pull "$DOCKER_USERNAME/olguides:$BUILD"
-
 sudo ../scripts/testApp.sh
