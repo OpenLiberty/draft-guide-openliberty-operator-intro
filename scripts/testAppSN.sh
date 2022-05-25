a5c53bc9cd527688e33fe0a7625f10cddeaa0f8d
Ryan Storey <ryanstorey999@gmail.com>
2022-05-25 15:57:15 +0100

    SN test

--- scripts/testAppSN.sh
+++ scripts/testAppSN.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 set -euxo pipefail
 
-mvn -q package
+mvn clean package
+
+kubectl api-resources --api-group=apps.openliberty.io | grep "apps.openliberty.io"
 
 docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
-
 docker build -t system:1.0-SNAPSHOT system/.
 
 NAMESPACE_NAME=$(bx cr namespace-list | grep sn-labs- | sed 's/ *$//g')
 echo "${NAMESPACE_NAME}"
+
 docker tag system:1.0-SNAPSHOT us.icr.io/"${NAMESPACE_NAME}"/system:1.0-SNAPSHOT
 docker push us.icr.io/"${NAMESPACE_NAME}"/system:1.0-SNAPSHOT
 
-sed -i 's=system:1.0-SNAPSHOT=us.icr.io/'"${NAMESPACE_NAME}"'/system:1.0-SNAPSHOT=g' deploy.yaml
-
+sed -i 's=system:1.0-SNAPSHOT=us.icr.io/'"${NAMESPACE_NAME}"'/system:1.0-SNAPSHOT\n  pullPolicy: Always\n  pullSecret: icr=g' deploy.yaml
 kubectl apply -f deploy.yaml
 
-sleep 60
+sleep 120
 
-kubectl get pods
+kubectl get OpenLibertyApplications | grep 'us.icr.io/'"${NAMESPACE_NAME}"'/system:1.0-SNAPSHOT' 
+kubectl describe olapps/system | grep "apps.openliberty.io/v1beta2" 
+kubectl describe pods
+kubectl port-forward svc/system 9080 &
 
-kubectl proxy &
+sleep 120
 
-NAMESPACE_NAME=$(bx cr namespace-list | grep sn-labs- | sed 's/ //g')
-SYSTEM_PROXY=localhost:8001/api/v1/namespaces/"$NAMESPACE_NAME"/services/system-service/proxy
-
-echo "$SYSTEM_PROXY"
-
-mvn failsafe:integration-test 
-mvn failsafe:verify
-
-curl http://"${SYSTEM_PROXY}"/system/properties
-
-kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)"
-
-kill "$(pidof kubectl)"
+curl -s http://localhost:9080/system/properties | jq
 
 kubectl delete -f deploy.yaml
+
+echo "Tests passed"
